961afa0d99113de92dc41d96f7b1c1805e2d0d3d
mikah <anhminhhoang13@gmail.com>
2018-04-28 10:04:04 -0700

    Localhost

--- private/credential/db_credentials.php
+++ private/credential/db_credentials.php
@@ -1,10 +1,7 @@
 <?php
-    // define("DB_SERVER", "127.0.0.1");
-    // define("DB_USER", "mikah");
-    // define("DB_PASS", "29071308");
-    // define("DB_NAME", "food");
-    define("DB_SERVER", "us-cdbr-iron-east-05.cleardb.net");
-    define("DB_USER", "bbc8e9e4cd239e");
-    define("DB_PASS", "6bab3bea");
+    define("DB_SERVER", "127.0.0.1");
+    define("DB_USER", "mikah");
+    define("DB_PASS", "29071308");
     define("DB_NAME", "food");
+
 ?>
