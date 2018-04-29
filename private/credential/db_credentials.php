e10c9befbff110f5b06f520b1553fef2d3d6f3fe
mikah <anhminhhoang13@gmail.com>
2018-04-28 20:56:04 -0700

    Update

--- private/credential/db_credentials.php
+++ private/credential/db_credentials.php
@@ -1,27 +1,27 @@
 <?php
-    
-    $connectstr_dbhost = '';
-    $connectstr_dbname = '';
-    $connectstr_dbusername = '';
-    $connectstr_dbpassword = '';
 
-    foreach ($_SERVER as $key => $value) {
-        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-            continue;
-        }
+    // $connectstr_dbhost = '';
+    // $connectstr_dbname = '';
+    // $connectstr_dbusername = '';
+    // $connectstr_dbpassword = '';
+    //
+    // foreach ($_SERVER as $key => $value) {
+    //     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+    //         continue;
+    //     }
+    //
+    // $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    // $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    // $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    // $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    // }
+    define("DB_SERVER", "127.0.0.1");
+    define("DB_USER", "YOUR NAME");
+    define("DB_PASS", "YOUR PASSWORD");
+    define("DB_NAME", "food");
 
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-    }
-    // define("DB_SERVER", "127.0.0.1");
-    // define("DB_USER", "mikah");
-    // define("DB_PASS", "29071308");
-    // define("DB_NAME", "food");
-
-    define("DB_SERVER", $connectstr_dbhost);
-    define("DB_USER", $connectstr_dbusername);
-    define("DB_PASS", $connectstr_dbpassword);
-    define("DB_NAME", $connectstr_dbname);
+    // define("DB_SERVER", $connectstr_dbhost);
+    // define("DB_USER", $connectstr_dbusername);
+    // define("DB_PASS", $connectstr_dbpassword);
+    // define("DB_NAME", $connectstr_dbname);
 ?>
