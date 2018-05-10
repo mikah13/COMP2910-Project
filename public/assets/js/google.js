2fdf70684f3b64e8f1997de8bac757b6de56efd7
Moontop <snail0525@naver.com>
2018-05-10 10:04:39 -0700

    Updating google button

--- public/assets/js/google.js
+++ public/assets/js/google.js
@@ -15,3 +15,13 @@ function onSignIn(googleUser) {
         }
     })
 };
+
+function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 220,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSignIn,
+      });
+    }
