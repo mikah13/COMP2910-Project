f17b3d07b1b896a70e682616f35345f1f4dab31d
mikah <anhminhhoang13@gmail.com>
2018-05-09 18:04:49 -0700

    update

--- public/menu.php
+++ public/menu.php
@@ -133,7 +133,6 @@
  	<script src="assets/js/jquery.scrollgress.min.js"></script>
  	<script src="assets/js/skel.min.js"></script>
  	<script src="assets/js/util.js"></script>
- 	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
  	<script src="assets/js/main.js"></script>
     <script src="assets/js/menu.js"></script>
 
