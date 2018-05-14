5ccee5f91fd21e80eec1fab1b1cb9d0c78841491
mikah <anhminhhoang13@gmail.com>
2018-05-13 17:20:40 -0700

    Update

--- public/assets/js/detail.js
+++ public/assets/js/detail.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
 
     let data = JSON.parse($('#data').text());
-    console.log(data);
     let totalCost = data.totalCost;
     let totalCal = data.totalCal;
     let arr = data.data
     arr.forEach(a=>{
-        console.log(a);
         let row = `<tr><td style="padding:.5rem">${a.title}</td><td style="padding:.5rem">$ ${a.price}</td><td style="padding:.5rem">${a.calorie} cal</td></tr>`;
         $('#tb-body').append(row);
     })
