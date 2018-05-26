b75309296ab9f7404d87173e179d037772a913ac
mikah <anhminhhoang13@gmail.com>
2018-05-26 14:29:46 -0700

    Merge branch 'master' of https://github.com/mikah13/COMP2910-Project

--- justextra/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.content').html("alo");
-    $.ajax({
-        url:'https://jperfect.azurewebsites.net/api/',
-
-    }).done(e=>{
-    $('.content').html(e);
-    })
-})
